12614c0ce8ab0c30eb9b4de2f8b35aa636fe489a
Tim Chopard <timchopard@pm.me>
2023-01-19 10:58:19 +0000

    completed lesson 3

--- blango/settings.py
+++ blango/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,6 +74,12 @@ TEMPLATES = [
         },
     },
 ]
+
+### Debug attempt
+TEMPALTE_DIRS = (
+  os.path.join(BASE_DIR, 'templates'),
+)
+### end
 
 WSGI_APPLICATION = 'blango.wsgi.application'
 
