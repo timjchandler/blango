fba2db3daff20e74f808f334f9743c745d5371b9
Tim Chopard <timchopard@pm.me>
2023-01-20 13:49:31 +0000

    finished security lesson

--- blango/settings.py
+++ blango/settings.py
@@ -107,6 +107,12 @@ class Dev(Configuration):
 #       }
 #   }
 
+  PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
 
   # Password validation
   # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -183,7 +189,7 @@ class Dev(Configuration):
               "level": "ERROR",
               "propagate": True,
           }
-      }
+      },
       "root": {
           "handlers": ["console"],
           "level": "DEBUG",
