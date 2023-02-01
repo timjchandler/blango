b8bb5215033bed68c911b80cd9cf25506b5ef9ba
Tim Chopard <timchopard@pm.me>
2023-02-01 12:20:49 +0000

    finished serializer lesson

--- blango/settings.py
+++ blango/settings.py
@@ -76,6 +76,7 @@ class Dev(Configuration):
       'allauth.account',
       'allauth.socialaccount',
       'allauth.socialaccount.providers.google',
+      'rest_framework',
   ]
 
   MIDDLEWARE = [
--- blango/urls.py
+++ blango/urls.py
@@ -42,6 +42,8 @@ urlpatterns = [
   path("accounts/", include("django_registration.backends.activation.urls")),
 
   path("accounts/", include("allauth.urls")),
+
+  path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
